9a86b58a947e01298a45ae585bfad9108e66a333
Nikhil Gupta <nikgupta06@gmail.com>
2022-04-11 23:39:35 -0500

    Attempt simultaneous client/server. Failed due to multiple network peers

--- Server.gd
+++ Server.gd
@@ -1,21 +1,27 @@
 extends Node
 
 
-# Instance variables
+# Instance variables - client and server
 var network: NetworkedMultiplayerENet = NetworkedMultiplayerENet.new()
-var ip: String = "127.0.0.1"
+
+# Instance variables - server
+var max_players: int = 100
 var port: int = 42069
+
+# Instance variables - client
+var ip: String = "127.0.0.1"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.connectToServer()
+	pass
 
 
 func connectToServer() -> void:
+	print("Connecting to '%s'..." % self.ip)
 	self.network.create_client(self.ip, self.port)
 	self.get_tree().set_network_peer(self.network)
-	print("Created client...")
+	print("Created client.")
 	
 	self.network.connect("connection_failed", self, "_on_connection_failed")
 	self.network.connect("connection_succeeded", self, "_on_connection_succeeded")
@@ -27,3 +33,20 @@ func _on_connection_failed() -> void:
 	
 func _on_connection_succeeded() -> void:
 	print("Connection established.")
+
+
+func startServer() -> void:
+	self.network.create_server(self.port, self.max_players)
+	self.get_tree().set_network_peer(self.network)
+	print("Server has started...")
+	
+	self.network.connect("peer_connected", self, "_on_peer_connected")
+	self.network.connect("peer_disconnected", self, "_on_peer_disconnected")
+
+
+func _on_peer_connected(peer_id: int) -> void:
+	print("Peer with id=%s connected." % peer_id)
+	
+
+func _on_peer_disconnected(peer_id: int) -> void:
+	print("Peer with id=%s disconnected." % peer_id)
--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -13,7 +13,7 @@ onready var back_btn = $MarginContainer/BackButton
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	self.select_connection_tree.visible = true
 	self.enter_server_tree.visible = false
 	self.back_btn.visible = false
@@ -24,21 +24,35 @@ func _ready():
 #	pass
 
 
-func _on_Local_pressed():
+func _on_Local_pressed() -> void:
+	# Create local server and connect to it as a peer
+	Server.startServer()
+	Server.connectToServer()
+	# Transition to actual game
 	self.get_tree().change_scene("res://scenes/Main.tscn")
 
 
-func _on_Connect_pressed():
+func _on_Connect_pressed() -> void:
 	self.select_connection_tree.visible = false
 	self.enter_server_tree.visible = true
 	self.back_btn.visible = true
 
 
-func _on_ConnectServer_pressed():
+func _on_ConnectServer_pressed() -> void:
+	# Do nothing if no server is defined
+	if not self.server_ip_text.text:
+		print("No server defined.")
+		return
+	
+	# Connect to server as a peer
 	print("Trying to connect to '%s' " % self.server_ip_text.text)
+	Server.ip = self.server_ip_text.text
+	Server.connectToServer()
+	# Transition to actual game
+	self.get_tree().change_scene("res://scenes/Main.tscn")
 
 
-func _on_BackButton_pressed():
+func _on_BackButton_pressed() -> void:
 	self.select_connection_tree.visible = true
 	self.enter_server_tree.visible = false
 	self.back_btn.visible = false
