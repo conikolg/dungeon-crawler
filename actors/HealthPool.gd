78b0441a5475437a4305c7608c3bbf5149da5478
Nikhil Gupta <nikgupta06@gmail.com>
2022-04-17 20:37:47 -0500

    Streamline signaling on health change

--- actors/HealthPool.gd
+++ actors/HealthPool.gd
@@ -12,26 +12,28 @@ export (int) var maximum_health = 100
 export (int) var health_regeneration = 1
 export (int) var health_regeneration_cutoff = 10
 var current_health: int setget set_health
-var _previous_health: int
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.current_health = self.maximum_health
-	self._previous_health = self.current_health
 
 
 # Called every frame.
 func _process(_delta: float) -> void:
-	# Emit signal that health has changed if it is different than in the last frame
-	if self.current_health != self._previous_health:
-		emit_signal("health_changed", self.current_health)
-	self._previous_health = self.current_health
+	pass
 
 
 func set_health(new_health: int) -> void:
-	current_health = int(clamp(new_health, 0, self.maximum_health))
-	if self.current_health == 0:
+	# Compute new health
+	new_health = int(clamp(new_health, 0, self.maximum_health))
+	# Did it change?
+	if current_health != new_health:
+		emit_signal("health_changed", new_health)
+	# Apply new health
+	current_health = new_health
+	# Did this run out of health?
+	if current_health == 0:
 		emit_signal("health_reached_zero")
 
 
