329a73dbdd5c2046e9d33458caf0f5eacb154ba3
Nikhil Gupta <nikgupta06@gmail.com>
2022-04-17 23:33:14 -0500

    Move world state management, time offset, and lerp factor calculations

--- Client.gd
+++ Client.gd
@@ -6,7 +6,11 @@ var peer: NetworkedMultiplayerENet
 var port: int = 42069
 var ip: String = "localhost"
 var server
-var last_server_update_time: int = -1
+
+var last_server_update_time: int 	# Timestamp of latest update recieved from the server
+var world_state_buffer: Array = []	# Buffer of world states, only i=1 in the past
+var render_time_offest: int = 50	# Render the world 50ms in the past
+var lerp_factor: float = 0			# Proportion of time between states at i=0 and i=1
 
 
 func _init() -> void:
@@ -21,16 +25,28 @@ func _init() -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	self.last_server_update_time = -1
 
 
 # Called every frame
 func _process(_delta: float) -> void:
+	# Allow network signals to work
 	if not self.custom_multiplayer:
 		return
 	if not self.custom_multiplayer.has_network_peer():
 		return
 	custom_multiplayer.poll();
+	
+	# Update lerp factor between server updates
+	if self.world_state_buffer.size() < 2:
+		return
+	# Render the players at this timestamp
+	var render_time: int = OS.get_system_time_msecs() - self.render_time_offest
+	# Advance buffer so that only the first (oldest) state is in the past
+	while self.world_state_buffer.size() > 2 and self.world_state_buffer[1]["time"] < render_time:
+		self.world_state_buffer.remove(0)
+	# Render in the right location
+	self.lerp_factor = float(render_time - self.world_state_buffer[0]["time"]) / float(self.world_state_buffer[1]["time"] - self.world_state_buffer[0]["time"])
 
 
 func connectToServer() -> void:
@@ -83,7 +99,9 @@ remote func client_receive_world_state(world_state: Dictionary) -> void:
 	var player_dict: Dictionary = world_state["players"]
 	var my_id = self.multiplayer.get_network_unique_id()
 	player_dict.erase(str(my_id))
-	player_dict["time"] = world_state["time"]
+	
+	# Record as state of the world
+	self.world_state_buffer.append(world_state)
 	
 	# Hand off the state of all players to the RemotePlayers node
 	self.get_node("/root/Main/RemotePlayers").update_players(player_dict)
--- managers/RemotePlayers.gd
+++ managers/RemotePlayers.gd
@@ -3,9 +3,6 @@ extends Node2D
 
 var remote_player_scene = preload("res://actors/RemotePlayer.tscn")
 
-var player_state_buffer: Array = []
-var render_time_offest: int = 50	# Render the world 50ms in the past
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,22 +11,19 @@ func _ready():
 
 # Called every frame
 func _process(_delta: float) -> void:
-	# Skip rendering if no diff is available
-	if self.player_state_buffer.size() < 2:
+	# Nothing to do if not enough states exist
+	if Client.world_state_buffer.size() < 2:
 		return
 	
-	# Render the players at this timestamp
-	var render_time: int = OS.get_system_time_msecs() - self.render_time_offest
-	# Advance buffer so that only the first (oldest) state is in the past
-	while self.player_state_buffer.size() > 2 and self.player_state_buffer[1]["time"] < render_time:
-		self.player_state_buffer.remove(0)
-	# Render in the right location
-	var interpolation: float = float(render_time - self.player_state_buffer[0]["time"]) / float(self.player_state_buffer[1]["time"] - self.player_state_buffer[0]["time"])
-	for peer_id in self.player_state_buffer[1].keys():
-		if str(peer_id) == "time":
-			continue	# Not actually a player
-		if not self.player_state_buffer[0].has(peer_id):
-			continue	# Can't lerp if nonexistent previously
+	# Get necessary data from Client
+	var prev_player_state: Dictionary = Client.world_state_buffer[0]["players"]
+	var future_player_state: Dictionary = Client.world_state_buffer[1]["players"]
+	
+	# Update for all players
+	for peer_id in future_player_state.keys():
+		# Can't lerp if nonexistent previously
+		if not prev_player_state.has(peer_id):
+			continue
 		
 		# Get player's node
 		var player_node = self.get_node_or_null("Player%s" % peer_id)
@@ -39,16 +33,13 @@ func _process(_delta: float) -> void:
 			player_node.name = "Player%s" % peer_id
 			self.add_child(player_node)
 		# Lerp player position and rotation
-		var pos: Vector2 = lerp(self.player_state_buffer[0][peer_id]["pos"], self.player_state_buffer[1][peer_id]["pos"], interpolation)
-		var rot: float = lerp_angle(self.player_state_buffer[0][peer_id]["rot"], self.player_state_buffer[1][peer_id]["rot"], interpolation)
+		var pos: Vector2 = lerp(prev_player_state[peer_id]["pos"], future_player_state[peer_id]["pos"], Client.lerp_factor)
+		var rot: float = lerp_angle(prev_player_state[peer_id]["rot"], future_player_state[peer_id]["rot"], Client.lerp_factor)
 		player_node.global_position = pos
 		player_node.rotation = rot
 
 
 func update_players(player_state: Dictionary) -> void:
-	# Add the new state to the buffer
-	self.player_state_buffer.append(player_state)
-	
 	# Delete players that do not exist in player_state
 	for child in self.get_children():
 		var child_peer_id: String = child.name.substr("Player".length())
